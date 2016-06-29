8ce751bb620c91078637cbb837f35b31c9b7039b
Tania <tania@efaber.net>
2016-06-29 13:36:02 +0200

    Adds EU (basque) translation

---
+++ js/languages/bootstrap-year-calendar.eu.js
@@ -0,0 +1,16 @@
+/**
+ * Basque translation for bootstrap-year-calendar
+ * Tania Rubio <taniarubiov@gmail.com>
+ */
+ 
+;(function($){
+	$.fn.calendar.dates['eu'] = {
+    days: ["Igandea", "Astelehena", "Asteartea", "Asteazkena", "Osteguna", "Ostirala", "Larunbata"],
+		daysShort: ["Igan", "Astel", "Astear", "Asteaz", "Oste", "Osti", "Lar"],
+		daysMin: ["Ig", "Al", "Ar", "Az", "Oe", "Oi", "La"],
+		months: ["Urtarrila", "Otsaila", "Martxoa", "Apirila", "Maiatza", "Ekaina", "Uztaila", "Abuztua", "Iraila", "Urria", "Azaroa", "Abendua"],
+		monthsShort: ["Urt", "Ots", "Mar", "Api", "Mai", "Eka", "Uzt", "Abu", "Ira", "Urr", "Aza", "Aben"],
+		weekShort: 'A',
+		weekStart: 1
+	};
+}(jQuery));
